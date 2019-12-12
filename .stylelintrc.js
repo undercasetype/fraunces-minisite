371eb7117f10a27febf1e42785e6d2c2d4664a04
Pascal Widdershoven <pascal@kabisa.nl>
2019-12-12 11:32:01 +0100

    Allow empty lines before css declarations
    So properties can be grouped, as was done in the original CSS.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -16,6 +16,17 @@ module.exports = {
 				severity: "warning",
 				ignore: ["font-unicode-range", "css-resize", "css-appearance"]
 			}
+		],
+		"declaration-empty-line-before": [
+			"always",
+			{
+				except: ["after-declaration", "first-nested"],
+				ignore: [
+					"after-comment",
+					"inside-single-line-block",
+					"after-declaration"
+				]
+			}
 		]
 	}
 };
