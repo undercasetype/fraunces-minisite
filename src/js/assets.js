ef75b392e4e30a1a842ecd261f70f1843348f9cf
Roel Nieskens <roel@pixelambacht.nl>
2020-01-10 12:07:25 +0100

    Add all stickers for opsz demo

--- src/js/assets.js
+++ src/js/assets.js
@@ -3,3 +3,5 @@ import "../img/old-paragraph.svg?external";
 import "../img/thank-you-for-shopping.svg?external";
 import "../img/lookin-good.svg?external";
 import "../img/not-so-much.svg?external";
+import "../img/happy.svg?external";
+import "../img/sad.svg?external";
