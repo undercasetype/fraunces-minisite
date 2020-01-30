5e9c763c7ff9f38564ab189740c5f0c7ac1835cc
Roel Nieskens <roel@pixelambacht.nl>
2020-01-30 13:10:58 +0100

    Add Open Graph/Twitter meta tags
    Note: no content yet

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,37 @@
 module.exports = {
 	title: "Fraunces",
 	description:
-		'Fraunces is a display, "Old Style" soft-serif typeface inspired by the mannerisms of early 20th century typefaces such as Windsor, Souvenir, and the Cooper Series.'
+		'Fraunces is a display, "Old Style" soft-serif typeface inspired by the mannerisms of early 20th century typefaces such as Windsor, Souvenir, and the Cooper Series.',
+
+	// More info: https://css-tricks.com/essential-meta-tags-social-media/
+	metatags: [
+		{
+			property: "og:title",
+			content: ""
+		},
+		{
+			property: "og:description",
+			content: ""
+		},
+		{
+			property: "og:image",
+			content: ""
+		},
+		{
+			property: "og:url",
+			content: ""
+		},
+		{
+			property: "og:site_name",
+			content: ""
+		},
+		{
+			name: "twitter:card",
+			content: ""
+		},
+		{
+			name: "twitter:image:alt",
+			content: ""
+		}
+	]
 };
