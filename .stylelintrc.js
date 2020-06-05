1df0a6f3bbe0d089a7942b07d79f079382c6cb9c
Roel Nieskens <roel@pixelambacht.nl>
2020-06-05 11:19:44 +0200

    Update to latest Specimen Skeleton
    Co-authored-by: Pascal Widdershoven <pascal@kabisa.nl>

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,20 @@ module.exports = {
 	extends: ["stylelint-config-standard", "stylelint-config-prettier"],
 	plugins: ["stylelint-no-unsupported-browser-features"],
 	rules: {
+		"selector-id-pattern": [
+			"^([a-z][a-z0-9]*)(-[a-z0-9]+)*$",
+			{
+				message:
+					"Selector should use lowercase and separate words with hyphens (selector-class-pattern)"
+			}
+		],
+		"selector-class-pattern": [
+			"^([a-z][a-z0-9]*)(-[a-z0-9]+)*$",
+			{
+				message:
+					"Selector should use lowercase and separate words with hyphens (selector-class-pattern)"
+			}
+		],
 		indentation: "tab",
 		"selector-pseudo-class-no-unknown": [
 			true,
@@ -9,23 +23,11 @@ module.exports = {
 				ignorePseudoClasses: ["global"]
 			}
 		],
-		"custom-property-empty-line-before": null,
 		"plugin/no-unsupported-browser-features": [
 			true,
 			{
 				severity: "warning",
 				ignore: ["font-unicode-range", "css-resize", "css-appearance"]
-			}
-		],
-		"declaration-empty-line-before": [
-			"always",
-			{
-				except: ["after-declaration", "first-nested"],
-				ignore: [
-					"after-comment",
-					"inside-single-line-block",
-					"after-declaration"
-				]
 			}
 		]
 	}
--- _tools/generateFontData.js
+++ _tools/generateFontData.js
@@ -1,3 +1,17 @@
+// Copyright 2019 Google LLC
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// 	https://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 const fs = require("fs");
 const util = require("util");
 const path = require("path");
@@ -5,7 +19,7 @@ const {
 	parseFontFile,
 	buildStylesheet,
 	buildFontJs
-} = require("specimen-boilerplate-support");
+} = require("specimen-skeleton-support");
 
 const srcDirectory = path.resolve(__dirname, "../", "src");
 const fontsDirectory = path.resolve(srcDirectory, "fonts");
@@ -55,7 +69,9 @@ const writeFontJs = async fontData => {
 };
 
 const findFirstFontFile = async directory => {
-	const fontFiles = await util.promisify(fs.readdir)(directory);
+	const fontFiles = (await util.promisify(fs.readdir)(directory)).filter(
+		f => path.extname(f) == ".woff2"
+	);
 
 	assert(
 		fontFiles.length > 0,
